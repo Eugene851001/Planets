ada874e9a096c591e83543c1136d97ebe8673c36
Yauheni Trakhanau <jackz1630@mail.ru>
2022-09-14 21:14:33 +0300

    Added Choice to rocket dialog

--- Assets/ScriptableObjects/Dialog/ChoiceNode.cs
+++ Assets/ScriptableObjects/Dialog/ChoiceNode.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.ScriptableObjects.Dialog
 {
-    public class ChoiceNode
+    [CreateAssetMenu(menuName = "Dialog/Choice node")]
+    public class ChoiceNode: ScriptableObject
     {
         public string Preview;
 
